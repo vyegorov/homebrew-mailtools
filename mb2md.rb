a0faea009c1c9857ed4c64394adab744f39e569d
Victor Yegorov <vyegorov@gmail.com>
2021-01-09 23:13:26 +0100

    Add revision to "mb2md"

--- mb2md.rb
+++ mb2md.rb
@@ -4,6 +4,7 @@ class Mb2md < Formula
   url "http://batleth.sapienti-sat.org/projects/mb2md/mb2md-3.20.pl.gz"
   sha256 "af45a9b5413a9fe49be0092e560485bf17efc50a4eb4a90744e380c4869f732f"
   license :public_domain
+  revision 1
 
   bottle :unneeded
 
